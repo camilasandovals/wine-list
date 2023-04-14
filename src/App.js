5cfe6212bb9c7d3ee8c8e8f7071b895633944911
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-04-14 11:28:13 -0400

    added wine list style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import WineList from './components/WineList';
 
 export default function App() {
   return (
     <main>
       <h1>Wine List</h1>
+      <WineList/>
     </main>
   );
 }
